c4e9a3ff0027c602cd9dcd34b87170f34edaea58
Isaac <43579566+isaacdyess@users.noreply.github.com>
2023-06-24 18:46:48 -0500

    Initial Commit

---
+++ RPSTrainer/RPSTrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RPSTrainerApp.swift
+//  RPSTrainer
+//
+//  Created by Isaac Dyess on 6/24/23.
+//
+
+import SwiftUI
+
+@main
+struct RPSTrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
